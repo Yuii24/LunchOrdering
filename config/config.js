5f3907a234aa1b5380dfc00819450afb22471fe7
Yuii24 <linian2040@gmail.com>
2025-03-03 01:27:26 +0800

    change config

--- config/config.js
+++ config/config.js
@@ -2,17 +2,19 @@ require('dotenv').config(); // 讀取 .env 檔案
 
 const config = {
   development: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.MYSQL_ROOT_PASSWORD || 'root',
-    database: process.env.DB_NAME || 'ordering',
-    host: process.env.DB_HOST || '127.0.0.1',
+    username: process.env.MYSQL_USERNAME || 'root',
+    password: process.env.MYSQL_PASSWORD || 'root',
+    database: process.env.MYSQL_DATABASE || 'ordering',
+    host: process.env.MYSQL_HOST || '127.0.0.1',
+    port: process.env.MYSQL_PORT || 4306,
     dialect: 'mysql'
   },
   production: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.MYSQL_ROOT_PASSWORD || 'root',
-    database: process.env.DB_NAME || 'ordering',
-    host: process.env.DB_HOST || '127.0.0.1',
+    username: process.env.MYSQL_USERNAME || 'root',
+    password: process.env.MYSQL_PASSWORD || 'root',
+    database: process.env.MYSQL_DATABASE || 'ordering',
+    host: process.env.MYSQL_HOST || '127.0.0.1',
+    port: process.env.MYSQL_PORT || 4306,
     dialect: 'mysql'
   }
 };
