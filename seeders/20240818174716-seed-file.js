499ab664d1a1092331162a372b7e92c4d16541ee
Yuii24 <linian2040@gmail.com>
2025-04-18 23:28:20 +0800

    change some setting

--- seeders/20240818174716-seed-file.js
+++ seeders/20240818174716-seed-file.js
@@ -1,19 +1,29 @@
 'use strict';
+require('dotenv').config(); // 讀取 .env 檔案
+const { User } = require('../models');
 const bcrypt = require('bcrypt')
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Users', [{
+    const passwordHash = await bcrypt.hash(process.env.ADMIN_PASSWORD, 10)
+
+    console.log('ADMIN_ACCOUNT:', process.env.ADMIN_ACCOUNT)
+    console.log('ADMIN_PASSWORD:', process.env.ADMIN_PASSWORD)
+    const userData = {
       id: 1,
-      employee_id: 1130000,
+      employeeId: process.env.ADMIN_ACCOUNT,
       name: 'root',
       email: 'root@example.com',
-      password: await bcrypt.hash('123456', 10),
-      is_admin: true,
+      password: passwordHash,
+      isAdmin: true,
       created_at: new Date(),
-      updated_at: new Date()
-    }], {})
+      updated_at: new Date(),
+    };
+
+    console.log(userData)
+    await User.upsert(userData);
   },
+
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Users', {})
+    await User.destroy({ where: { id: 1 } });
   }
 };
